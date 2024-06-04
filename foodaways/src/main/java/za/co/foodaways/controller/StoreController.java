3da783027407a78a7574be4f3965d7d44e71bfaf
BongaGougota0 <bonga_gougota@outlook.com>
2024-06-04 20:12:43 +0200

    add skeleton html page for displaying orders + empty js

--- foodaways/src/main/java/za/co/foodaways/controller/StoreController.java
+++ foodaways/src/main/java/za/co/foodaways/controller/StoreController.java
@@ -1,12 +1,33 @@
 package za.co.foodaways.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StoreController {
-    @RequestMapping(value = "/store-registration")
+    @RequestMapping(value = "/store-orders")
     public String registerStore(){
-        return "index.html";
+        return "order.html";
+    }
+
+    @RequestMapping(value = "/records")
+    public String records(){
+        return "orders.html";
+    }
+
+    @RequestMapping(value = "/update-status")
+    public String updateOrderStatus(){
+        return "redirect:/records";
+    }
+
+    @RequestMapping(value = "/accept")
+    public String acceptOrder(){
+        return "redirect:/records";
+    }
+
+    @RequestMapping(value = "/decline")
+    public String declineOrder(){
+        return "redirect:/records";
     }
 }
