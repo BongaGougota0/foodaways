e7873414db1778c3c9cdb289f276ca9f172bdf79
BongaGougota0 <bonga_gougota@outlook.com>
2024-08-03 22:30:50 +0200

    add custom onSuccessLogin redirect based on granted_authorities

---
+++ foodaways/src/main/java/za/co/foodaways/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,33 @@
+package za.co.foodaways.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        String userDestinationOnSuccesslogin = determineUserSuccessLoginView(authentication);
+        response.sendRedirect(userDestinationOnSuccesslogin);
+    }
+
+    private String determineUserSuccessLoginView(Authentication authentication) {
+        if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))){
+            return "/foodaways-admin";
+        } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CUSTOMER"))) {
+            return "/index";
+        } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_STORE_OWNER"))) {
+            return "/store-manager";
+        }
+        return "/";
+    }
+}
