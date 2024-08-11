2cdef6c735cc9c8d7b93d0d8ecf7d8f8293a4109
BongaGougota0 <bonga_gougota@outlook.com>
2024-08-11 18:02:25 +0200

    adding user on registration

--- foodaways/src/main/java/za/co/foodaways/service/StoreUserService.java
+++ foodaways/src/main/java/za/co/foodaways/service/StoreUserService.java
@@ -3,23 +3,26 @@ package za.co.foodaways.service;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import za.co.foodaways.model.Roles;
 import za.co.foodaways.model.StoreUser;
+import za.co.foodaways.repository.RoleRepository;
 import za.co.foodaways.repository.StoreUserRepository;
 
 @Service
 public class StoreUserService {
     @Autowired
     StoreUserRepository storeUserRepository;
+    @Autowired
+    RoleRepository roleRepository;
 
     public StoreUser findUserByEmail(String userEmail){
         return storeUserRepository.findByEmail(userEmail);
     }
 
-
     public int createUser(StoreUser user){
-        user.setRoleId(2);
-        StoreUser User_ = storeUserRepository.save(user);
-        if(User_.getId() > 0){
+        user.setRole(roleRepository.findRoleByRoleName("CUSTOMER"));
+        StoreUser savedUser = storeUserRepository.save(user);
+        if(savedUser.userId > 0){
             return 1;
         }
         return 0;
