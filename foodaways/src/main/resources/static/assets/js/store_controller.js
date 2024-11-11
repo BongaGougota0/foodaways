7153e8d937dcd657a6ab20baa11307763b4d3459
Bonga Gougota <bonga_gougota@outlook.com>
2024-11-11 19:11:45 +0200

    change and add new files. store js and html

---
+++ foodaways/src/main/resources/static/assets/js/store_controller.js
@@ -0,0 +1,41 @@
+function fetchStoreOrders(storeId) {
+    const eventSource = new EventSource(`/orders/${storeId}`);
+    const ordersContainer = document.getElementById('orders-container');
+    eventSource.onmessage = function(event) {
+        const order = JSON.parse(event.data);
+        const orderDiv = document.createElement('div');
+        orderDiv.innerHTML = `
+            <div class="column">
+                <h3>Order Info</h3>
+                <p>Order #: ${order.orderId}</p>
+                <p>Status: ${order.orderStatus}</p>
+            </div>
+            <div class="middle">
+                <h3>Order Details</h3>
+                <p>Product: ${order.orderItems}</p>
+                <p>Quantity: </p>
+                <p>Price: </p>
+            </div>
+            <div class="column">
+                <h3>Customer Info</h3>
+                <p>Phone: </p>
+                <p>Delivery: </p>
+                <div>
+                    <button id="incoming-order" class="">Accept Order</button>
+                </div>
+                <!-- Dynamically change the text of this button on click -->
+                <div id="order-status" class="">
+                    <button id="order-button" class="">Order Status</button>
+                </div>
+            </div>
+        `;
+        ordersContainer.appendChild(orderDiv);
+    };
+    // Handle any errors from the EventSource
+    eventSource.onerror = function(error) {
+        console.error('Error receiving order updates:', error);
+        eventSource.close();
+    };
+}
+
+window.onload = fetchStoreOrders([[${storeId}]]);
--- foodaways/src/main/resources/static/assets/js/store_manager_functions.js
+++ foodaways/src/main/resources/static/assets/js/store_manager_functions.js
@@ -2,12 +2,10 @@
     function openModal() {
         document.getElementById('productModal').style.display = 'flex';
     }
-
     // Close the modal
     function closeModal() {
         document.getElementById('productModal').style.display = 'none';
     }
-
     // Close modal when clicking outside of it
     window.onclick = function (event) {
         if (event.target === document.getElementById('productModal')) {
